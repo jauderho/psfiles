8860e86eeb75cf45942edf6f63731729471a75f8
Jauder Ho <jauderho@users.noreply.github.com>
2024-04-18 23:07:49 -0700

    Enable BBRv2
    Signed-off-by: Jauder Ho <jauderho@users.noreply.github.com>

---
+++ BBRv2Enable.ps1
@@ -0,0 +1,60 @@
+
+<#
+.SYNOPSIS
+   Enable BBRv2 as the TCP congestion provider
+
+.DESCRIPTION
+   Enable BBRv2 as the TCP congestion provider
+
+.NOTES
+   Created by Jauder Ho
+   Last modified 11/1/2019
+   https://www.carumba.com
+
+   BSD License
+
+   Pull requests are welcome.
+
+.LINK
+   https://docs.microsoft.com/en-us/azure/active-directory/hybrid/reference-connect-tls-enforcement
+   https://jorgequestforknowledge.wordpress.com/category/windows-azure-active-directory/azure-ad-application-proxy-connector/
+   https://support.microsoft.com/en-us/help/3135244/tls-1.2-support-for-microsoft-sql-server
+   https://www.hass.de/content/setup-your-iis-ssl-perfect-forward-secrecy-and-tls-12
+   https://msdn.microsoft.com/en-us/library/aa374757(VS.85).aspx
+   https://blogs.technet.microsoft.com/askpfeplat/2017/11/13/demystifying-schannel/
+   https://support.microsoft.com/en-us/help/3140245/update-to-enable-tls-1-1-and-tls-1-2-as-default-secure-protocols-in-wi
+#>
+
+# Elevate as needed
+# https://superuser.com/questions/108207/how-to-run-a-powershell-script-as-administrator
+param([switch]$Elevated)
+
+function Test-Admin {
+  $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+  $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false) {
+    if ($elevated) {
+        # tried to elevate, did not work, aborting
+    }
+    else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+
+    exit
+}
+
+Write-Output 'Running with full privileges...'
+
+function Enable-BBRv2 {
+  Set-NetTCPSetting -SettingName "Internet" -CongestionProvider BBR2
+  Set-NetTCPSetting -SettingName "Datacenter" -CongestionProvider BBR2
+  Set-NetTCPSetting -SettingName "Compat" -CongestionProvider NewReno
+  Set-NetTCPSetting -SettingName "InternetCustom" -CongestionProvider BBR2
+  Set-NetTCPSetting -SettingName "DatacenterCustom" -CongestionProvider BBR2
+
+  Write-Output 'BBRv2 has been enabled'
+}
+
+Enable-BBRv2
