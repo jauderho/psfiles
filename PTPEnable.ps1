f13f87491d0ddf071bb16d1b1772715cc7a09d6d
Jauder Ho <jauderho@users.noreply.github.com>
2026-09-22 00:58:36 -0700

    Add gates for PTPEnable.ps1
    Signed-off-by: Jauder Ho <jauderho@users.noreply.github.com>

--- PTPEnable.ps1
+++ PTPEnable.ps1
@@ -20,29 +20,61 @@
 .LINK
    https://techcommunity.microsoft.com/t5/networking-blog/windows-subsystem-for-linux-for-testing-windows-10-ptp-client/ba-p/389181
    https://github.com/microsoft/W32Time/tree/master/Precision%20Time%20Protocol/docs
+.PARAMETER Force
+   Skip the PTP capability check and enable PTP anyway.
+.PARAMETER DryRun
+   Check PTP capability and report what would happen without enabling.
+   Common -Verbose switch adds per-adapter detail.
+
 #>
 
 # Elevate as needed
 # https://superuser.com/questions/108207/how-to-run-a-powershell-script-as-administrator
-param([switch]$Elevated)
+[CmdletBinding()] param([switch]$Elevated, [switch]$Force, [switch]$DryRun)
 
 function Test-Admin {
    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 }
 
-if ((Test-Admin) -eq $false) {
+if ((-not $DryRun) -and ((Test-Admin) -eq $false)) {
    if ($elevated) {
       # tried to elevate, did not work, aborting
    }
    else {
-      Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+      $argList = '-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition)
+      if ($Force) { $argList += ' -Force' }
+      if ($VerbosePreference -ne 'SilentlyContinue') { $argList += ' -Verbose' }
+      Start-Process powershell.exe -Verb RunAs -ArgumentList $argList
    }
 
    exit
 }
 
-Write-Output 'Running with full privileges...'
+if ($DryRun) { Write-Output 'Running in dry-run mode, no changes will be made...' } else { Write-Output 'Running with full privileges...' }
+
+function Test-PtpCapable {
+   # Keyword presence shows the driver exposes the timestamp switch, not that the silicon timestamps correctly; update the driver first on a miss
+   $capable = @()
+   try {
+      $adapters = Get-NetAdapter -Physical -ErrorAction Stop | Where-Object { $_.Status -ne 'Disabled' }
+   }
+   catch {
+      return $capable
+   }
+   Write-Verbose ('Probing {0} adapter(s) for *PtpHardwareTimestamp / *SoftwareTimestamp.' -f @($adapters).Count)
+   foreach ($adapter in $adapters) {
+      Write-Verbose ('Checking {0} ({1}, {2}).' -f $adapter.Name, $adapter.InterfaceDescription, $adapter.Status)
+      foreach ($keyword in '*PtpHardwareTimestamp', '*SoftwareTimestamp') {
+         if ($null -ne (Get-NetAdapterAdvancedProperty -Name $adapter.Name -RegistryKeyword $keyword -ErrorAction SilentlyContinue)) {
+            Write-Verbose ('{0} reports {1}.' -f $adapter.Name, $keyword)
+            $capable += $adapter
+            break
+         }
+      }
+   }
+   return $capable
+}
 
 function Enable-PTP {
    # Enable PTP. Make sure to define what the PTP master should be
@@ -64,4 +96,28 @@ function Enable-PTP {
    Write-Output 'PTP has been enabled.'
 }
 
-Enable-PTP
+if ($Force) {
+   Write-Output '-Force specified, skipping PTP capability check.'
+}
+else {
+   Write-Verbose ('-Force: {0}, -DryRun: {1}.' -f $Force, $DryRun)
+   $capable = @(Test-PtpCapable | Where-Object { $null -ne $_ })
+   if ($capable.Count -eq 0) {
+      $intelFound = @(Get-NetAdapter -Physical -ErrorAction SilentlyContinue | Where-Object { $_.InterfaceDescription -match 'Intel' }).Count -gt 0
+      if ($intelFound) {
+         Write-Error 'No PTP-capable interfaces found. Intel NIC detected but it does not report *PtpHardwareTimestamp; update the driver and retry, or rerun with -Force to override.'
+      }
+      else {
+         Write-Error 'No PTP-capable interfaces found. PTP needs a NIC reporting *PtpHardwareTimestamp (typically Intel I210/I211/I350/X550/X710/E810) or *SoftwareTimestamp; rerun with -Force to override.'
+      }
+      exit 1
+   }
+   Write-Output ('PTP-capable interface(s): {0}' -f (($capable | ForEach-Object { $_.Name }) -join ', '))
+}
+if ($DryRun) {
+   if ($Force) { Write-Output 'Dry run: PTP would be enabled (capability check skipped).' }
+   else { Write-Output 'Dry run: capability check passed, PTP was not enabled.' }
+}
+else {
+   Enable-PTP
+}
